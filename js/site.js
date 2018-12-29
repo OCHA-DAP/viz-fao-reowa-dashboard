8e0e27b01d5233b81cd50fc5d4cfdb92dae0dde9
ndongamadu <ndongamadou@gmail.com>
2018-12-29 17:24:11 +0000

    c3 charts, map colors, restructure

--- js/site.js
+++ js/site.js
@@ -46,12 +46,12 @@ dc.dataCount('#count-info')
 activityChart
             .width(400)
             .height(310) 
-    .margins({
-            top: 10,
-            right: 30,
-            bottom: 30,
-            left: 50
-          })
+            .margins({
+              top: 5,
+              right: 50,
+              bottom: 20,
+              left: 10
+            })
             .dimension(activityDimension)
             .group(activityGroup)
             .elasticX(true)
@@ -65,13 +65,13 @@ activityChart
   //rowChart financement par pays
   fundChart
             .width(400)
-            .height(310) 
-    .margins({
-            top: 10,
-            right: 30,
-            bottom: 30,
-            left: 50
-          })
+            .height(310)
+            .margins({
+              top: 5,
+              right: 50,
+              bottom: 20,
+              left: 10
+            })
             .dimension(fundDimension)
             .group(fundGroup)
             .elasticX(true)
@@ -82,15 +82,18 @@ activityChart
             .colors('#4169E1')
             .colorAccessor(function(d, i){return 0;})
             .xAxis().ticks(4);
-  //Map          
-   map.width($('#mapChart').width())
+  //Map
+  var mapColors = ['#DDDDDD','#4CCDFF', '#9AC7F3','#6787E7', '#033E8C'];//['#DDDDDD', '#fff7bc', '#ffeda0', '#fec44f', '#d95f0e'];
+   map
+      // .width($('#map').width())
+      .width(400)
        .height(300)
        .dimension(mapDimension)
        .group(mapGroup)
        .center([27.85,85.1])
-       .zoom(8) 
+       .zoom(8)
        .geojson(vargeodata)
-       .colors(['#DDDDDD', '#fff7bc', '#ffeda0', '#fec44f', '#d95f0e'])
+       .colors(mapColors)
        .colorDomain([0,5])
        .colorAccessor(function (d){
         var c = 0
@@ -112,10 +115,8 @@ activityChart
            +'<h6>'+'Nombre de projets: '+d.properties['projet']+ '</h6>'+'<h6>'
             +'Ménages bénéficiaires'+'</h6>';
        })
-          
         .renderPopup(true);
-       
-       
+
       dc.renderAll();
 
       var map = map.map({ 
@@ -132,8 +133,7 @@ activityChart
             .dragging.disable();
       }
         map.keyboard.disable();
-    
-     
+
       function genLookup(geojson) {
         var lookup = {} ;
         geojson.features.forEach(function (e) {
@@ -141,6 +141,86 @@ activityChart
         });
         return lookup ;
       }
+}
+var sortData = function(d1, d2) {
+    if (d1.key > d2.key) return 1;
+    if (d1.key < d2.key) return -1;
+    return 0;
+};
+
+function generateC3Charts (argument) {
+  var cf = crossfilter(argument);
+  var dim = cf.dimension(function(d){ return [d['#activity+name'], d['#country+name'], d['#activity+type']]; });
+  var grp = dim.group().reduceSum(function(d){ return d['#value']; }).top(Infinity).sort(sortData);
+
+  var indics = cf.dimension(function(d){ return d['#activity+name']; });
+  var gp = indics.group().top(Infinity);
+
+  var indicatorNames = [];
+  for (var i = gp.length - 1; i >= 0; i--) {
+    indicatorNames.push(gp[i].key);
+  }
+  $('#c3chart').html('');
+  for (var i = 0; i < indicatorNames.length; i++) {
+    //i = nom de l'indicateur en cours
+    $('#c3chart').append('<div class="col-md-4"><h5>'+indicatorNames[i]+'</h5><div id="chart'+i+'"></div></div>');
+    let qteDistribue = ['Atteint'],
+        qtePrevue = ['Cible'],
+        countries = ['x'];
+    for (var j = 0; j < grp.length; j++) {
+      if (grp[j].key[0] == indicatorNames[i]) {
+        grp[j].key[2] == "Cible" ? qtePrevue.push(grp[j].value): qteDistribue.push(grp[j].value);
+        let test = 0;
+        for (p in countries){
+          grp[j].key[1]==countries[p] ? test++ : null;
+        }
+        test === 0 ? countries.push(grp[j].key[1]) : null;
+      }
+    } //end of for (j)
+
+    i == 5 ? $('#c3chart').append('<div class="clearfix"></div>'): null;
+    generateC3(countries,qteDistribue,qtePrevue,i);
+  } //end of for (i)
+
+} //end of generateC3Charts
+
+var blueColor = '#6787E7',
+    greenColor = '#6AA84F';
+
+function generateC3(x, data1, data2, bind) {
+  var xAxis = x,
+      cible = data1,
+      atteint = data2;
+
+  var chart =  c3.generate({
+                bindto: '#chart'+bind,
+                data: {
+                    x: 'x',
+                    type: 'bar',
+                    columns: [xAxis, cible, atteint],
+                },
+                color: {
+                    pattern: [blueColor, greenColor]
+                },
+                axis: {
+                    x: {
+                        type: 'category',
+                    },
+                    y: {
+                        tick:{
+                          count: 6
+                        }
+                    }
+                },
+                size: {
+                    height: 250
+                },
+                padding: {left:20, right: 10},
+                legend: {
+                  hide: true
+                },
+    });
+  // $('#chart'+bind).data('chartObj', chart);
 }
 
 /*var dataCall = $.ajax({
@@ -160,13 +240,25 @@ var geomCall = $.ajax({
     dataType: 'json',
 });
 
-$.when(dataCall, geomCall).then(function(dataArgs, geomArgs){
+// debut Amadu's code
+
+var data2Call = $.ajax({
+  type: 'GET',
+  url: 'https://proxy.hxlstandard.org/data.json?strip-headers=on&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1_csDhgYdfvgaumE8uqKBRmh_JEpyem3XFuXuJ9WvbNw%2Fedit%23gid%3D785657249&force=on',
+  dataType: 'JSON'
+
+});
+
+$.when(dataCall, geomCall, data2Call).then(function(dataArgs, geomArgs, data2Args){
     var geom = geomArgs[0];
     geom.features.forEach(function(e){
         e.properties['country_code'] = String(e.properties['country_code']);
     });
     var dat = hxlProxyToJSON(dataArgs[0]);
     generateringComponent(dat,geom);
+
+    var data = hxlProxyToJSON(data2Args[0]);
+    generateC3Charts(data);
 
 });
 
